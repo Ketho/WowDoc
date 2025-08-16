8ae7aa7490d91ea23ac233755246ed4ab472ded6
Ketho <sghs17@outlook.com>
2025-08-16 03:40:44 +0200

    create namespace redirects

---
+++ Pywikibot/projects/20250816_namespace/namespace.py
@@ -0,0 +1,52 @@
+from lupa import LuaRuntime
+import pywikibot
+
+site = pywikibot.Site("en", "warcraftwiki")
+
+lua = LuaRuntime(unpack_returned_tuples=True)
+
+import requests
+
+def read_lua(path):
+	if path.startswith('https://'):
+		response = requests.get(path)
+		response.raise_for_status()
+		lua_code = response.text
+	else:
+		with open(path, 'r', encoding='utf-8') as f:
+			lua_code = f.read()
+
+	wrapped_code = f"""
+	function __py_loader()
+		{lua_code}
+	end
+	"""
+
+	lua.execute(wrapped_code)
+	result = lua.eval("__py_loader()")
+	return result
+
+def get_namespaces(result):
+	global_api = result[1]
+
+	apis = set()
+	for value in global_api.values():
+		if isinstance(value, str) and value.startswith('C_'):
+			namespace = value.split('.')[0]
+			apis.add(namespace)
+	apis = sorted(apis)
+	return apis
+
+def save_page(title, text, summary):
+	page = pywikibot.Page(site, title)
+	page.text = text
+	page.save(summary=summary)
+
+def main():
+	lua_table = read_lua("https://raw.githubusercontent.com/Ketho/BlizzardInterfaceResources/refs/heads/mainline/Resources/GlobalAPI.lua")
+	namespaces = get_namespaces(lua_table)
+	for name in namespaces:
+		save_page(name, f"#REDIRECT [[Category:API_namespaces/{name}]]", "Redirect to namespace category")
+
+if __name__ == "__main__":
+	main()
