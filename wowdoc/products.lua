a0f387c9c15d120a09704fdfdb542696692a2fd7
Ketho <sghs17@outlook.com>
2026-01-25 16:41:41 +0100

    update blizzres branches

--- wowdoc/products.lua
+++ wowdoc/products.lua
@@ -30,17 +30,17 @@ local m = {}
 ---|"ptr2"
 
 ---@alias BlizzResBranch string
+---|"live"
+---|"beta"
 ---|"classic_anniversary"
----|"mainline"
----|"mainline_ptr"
----|"mainline_beta"
----|"mists"
----|"mists_ptr"
----|"mists_beta"
----|"cata"
----|"cata_ptr"
----|"vanilla"
----|"vanilla_ptr"
+---|"classic_beta"
+---|"classic_era_ptr"
+---|"classic_era"
+---|"classic_ptr"
+---|"classic_titan"
+---|"classic"
+---|"ptr"
+---|"ptr2"
 ---
 ---@alias GameType
 ---|"mainline"
@@ -83,15 +83,17 @@ m.tact_gethe = {
 
 ---@type table<TactProduct, BlizzResBranch>
 m.blizzres_branch = {
-	wow = "mainline",
-	wowt = "mainline_ptr",
-	wowxptr = "mainline_ptr",
-	wow_beta = "mainline_beta",
-	wow_classic = "mists",
-	wow_classic_ptr = "mists_ptr",
-	wow_classic_beta = "mists_beta",
-	wow_classic_era = "vanilla",
-	wow_classic_era_ptr = "vanilla_ptr",
+	wow = "live",
+	wow_anniversary = "classic_anniversary",
+	wow_beta = "beta",
+	wow_classic = "classic",
+	wow_classic_beta = "classic_beta",
+	wow_classic_era = "classic_era",
+	wow_classic_era_ptr = "classic_era_ptr",
+	wow_classic_ptr = "classic_ptr",
+	wow_classic_titan = "classic_titan",
+	wowt = "ptr",
+	wowxptr = "ptr2",
 }
 
 ---@type table<GameType, GetheBranch>
