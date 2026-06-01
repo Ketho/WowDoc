ba03c19ba552874dba0f9463b84bce7a95564688
Ketho <sghs17@outlook.com>
2026-06-02 01:33:38 +0200

    get recursive structure info

--- Pages/API_types/find_type.lua
+++ Pages/API_types/find_type.lua
@@ -1,82 +1,131 @@
 local tablelib = require("wowdoc.util.table")
+local table_sort = require("wowdoc.util.table_sort")
 local loader = require("wowdoc.loader")
 local apilink = require("wowdoc.namingway.wiki.apilink")
+local naming = require("wowdoc.namingway.naming")
+
+local function GetActualType(v)
+	return v.InnerType or v.Type
+end
 
 local function FindFunctionTypes(name)
-    local t = {}
-    for _, v in pairs(APIDocumentation.functions) do
-        local found
-        if v.Arguments then
-            for _, v2 in pairs(v.Arguments) do
-                if v2.Type == name then
-                    found = true
-                end
-            end
-        end
-        if v.Returns then
-            for _, v2 in pairs(v.Returns) do
-                if v2.Type == name then
-                    found = true
-                end
-            end
-        end
-        if found then
-            table.insert(t, v)
-        end
+	local t = {}
+	for _, v in pairs(APIDocumentation.functions) do
+		local found
+		if v.Arguments then
+			for _, v2 in pairs(v.Arguments) do
+				if GetActualType(v2) == name then
+					found = true
+				end
+			end
+		end
+		if v.Returns then
+			for _, v2 in pairs(v.Returns) do
+				if GetActualType(v2) == name then
+					found = true
+				end
+			end
+		end
+		if found then
+			table.insert(t, v)
+		end
 
-    end
-    return t
+	end
+	return t
 end
 
 local function FindEventTypes(name)
-    local t = {}
-    for _, v in pairs(APIDocumentation.events) do
-        local found
-        if v.Payload then
-            for _, v2 in pairs(v.Payload) do
-                local actualType = v2.InnerType or v2.Type
-                if actualType == name then
-                    found = true
-                end
-            end
-        end
-        if found then
-            table.insert(t, v)
-        end
-    end
-    return t
+	local t = {}
+	for _, v in pairs(APIDocumentation.events) do
+		local found
+		if v.Payload then
+			for _, v2 in pairs(v.Payload) do
+				if GetActualType(v2) == name then
+					found = true
+				end
+			end
+		end
+		if found then
+			table.insert(t, v)
+		end
+	end
+	return t
 end
 
 local function FindTableTypes(name)
-    local t = {}
-    for _, v in pairs(APIDocumentation.tables) do
-        local found
-        if v.Fields then
-            for _, v2 in pairs(v.Fields) do
-                local actualType = v2.InnerType or v2.Type
-                if actualType == name then
-                    found = true
-                end
-            end
-        end
-        if found then
-            table.insert(t, v)
-        end
-    end
-    return t
+	local t = {}
+	for _, v in pairs(APIDocumentation.tables) do
+		local found
+		if v.Fields then
+			for _, v2 in pairs(v.Fields) do
+				if GetActualType(v2) == name then
+					found = true
+				end
+			end
+		end
+		if found then
+			table.insert(t, v)
+		end
+	end
+	return t
+end
+
+local function GetTypeOrigin(name, map)
+	local locations = tablelib.CombineList(
+		FindFunctionTypes(name),
+		FindEventTypes(name),
+		FindTableTypes(name)
+	)
+	for _, v in pairs(locations) do
+		if v.Type ~= "Structure" then
+			local proper_name = naming:GetProperName(v)
+			map[proper_name] = v
+		else
+			GetTypeOrigin(v.Name, map)
+		end
+	end
+end
+
+local function GetStructureLine(name, t)
+	local map = {}
+	GetTypeOrigin(name, map)
+	if next(map) then
+		table.insert(t, " -> <small>")
+		local sorted = table_sort.ByValue(map, apilink.SortApiLink)
+		local r = {}
+		for _, v in pairs(sorted) do
+			table.insert(r, apilink:GetWikiTemplate(v, true))
+		end
+		table.insert(t, table.concat(r, ", "))
+		table.insert(t, "</small>")
+	end
+end
+
+local function PrintMainListing(name)
+	local locations = tablelib.CombineList(
+		FindFunctionTypes(name),
+		FindEventTypes(name),
+		FindTableTypes(name)
+	)
+	table.sort(locations, apilink.SortApiLink)
+	for _, v in pairs(locations) do
+		local t = {}
+		if v.Type == "Structure" then
+			table.insert(t, "* ") -- more visible due to cluttered output
+		else
+			table.insert(t, ": ")
+		end
+		table.insert(t, apilink:GetWikiTemplate(v))
+		if v.Type == "Structure" then
+			GetStructureLine(v.Name, t)
+		end
+		print(table.concat(t))
+	end
 end
 
 local function main(name)
-    loader:LoadDocumentation()
-    local locations = tablelib.CombineList(
-        FindFunctionTypes(name),
-        FindEventTypes(name),
-        FindTableTypes(name)
-    )
-    table.sort(locations, apilink.SortApiLink)
-    for k, v in pairs(locations) do
-        print(string.format(": %s", apilink:GetWikiTemplate(v)))
-    end
+	loader:LoadDocumentation()
+	PrintMainListing(name)
 end
 
 main("WOWGUID")
--- wowdoc/namingway/wiki/apilink.lua
+++ wowdoc/namingway/wiki/apilink.lua
@@ -2,21 +2,27 @@ local naming = require("wowdoc.namingway.naming")
 require("warcraftwiki.page")
 local m = {}
 
-function m:GetWikiTemplate(doc)
+function m:GetWikiTemplate(doc, plain)
 	local t = {}
 	table.insert(t, "apilink")
 	table.insert(t, "t="..doc.Type)
 	table.insert(t, naming:GetProperName(doc))
 	if doc.Type == "Function" then -- also includes ScriptObject methods
-		if doc.Arguments and #doc.Arguments > 0 then
-			table.insert(t, "arg="..WarcraftWiki:GetFunctionArguments(doc))
-		end
-		if doc.Returns and #doc.Returns > 0 then
-			table.insert(t, "ret="..WarcraftWiki:GetFunctionReturns(doc))
+		if not plain then
+			if doc.Arguments and #doc.Arguments > 0 then
+				table.insert(t, "arg="..WarcraftWiki:GetFunctionArguments(doc))
+			end
+			if doc.Returns and #doc.Returns > 0 then
+				table.insert(t, "ret="..WarcraftWiki:GetFunctionReturns(doc))
+			end
+		else
+			table.insert(t, "noparens=1")
 		end
 	elseif doc.Type == "Event" then
-		if doc.Payload then
-			table.insert(t, "payload="..doc:GetPayloadString(false, false))
+		if not plain then
+			if doc.Payload then
+				table.insert(t, "payload="..doc:GetPayloadString(false, false))
+			end
 		end
 	end
 	return string.format("{{%s}}", table.concat(t, "|"))
